a7c6fe0799fdf8a48db94adc463e978487f49de5
imbradyboy <55708624+imbradyboy@users.noreply.github.com>
2022-06-24 14:25:50 -0400

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh -l
 
-sh -c "echo Hello world my name is $INPUT_MY_NAME and I like to eat $INPUT_FAVOURITE_FOOD"
+sh -c "echo Hello world my name is $INPUT_MY_NAME and I like to eat $FAVOURITE_FOOD"
